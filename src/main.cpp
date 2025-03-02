8f3c47a19cdfa52f05544725fa04cff6587efe22
Andreo Van Onckelen <andreo141mlg@gmail.com>
2025-03-02 23:10:04 +0100

    Firts working code. Displays on screen: - Co2 levels - Temperature - Humidity

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,156 @@
 #include <TFT_eSPI.h>  // Include the TFT_eSPI library
+#include <SensirionI2cScd4x.h>
+#include <Wire.h>
 
-TFT_eSPI tft = TFT_eSPI(); // Create TFT object
+#ifdef NO_ERROR
+#undef NO_ERROR
+#endif
+#define NO_ERROR 0
 
+TFT_eSPI tft; 
+SensirionI2cScd4x sensor;
+
+static char errorMessage[64];
+static int16_t error;
+
+void PrintUint64(uint64_t& value) {
+  Serial.print("0x");
+  Serial.print((uint32_t)(value >> 32), HEX);
+  Serial.print((uint32_t)(value & 0xFFFFFFFF), HEX);
+}
 
 void setup() {
-  tft.begin(); // Initialize TFT
-  tft.setRotation(3); // Set rotation if needed
+  // Sensor Initialization
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(100);
+  }
+  Wire.begin();
+  sensor.begin(Wire, SCD41_I2C_ADDR_62);
+  uint64_t serialNumber = 0;
+  delay(30);
+  // Ensure sensor is in a clean state
+  error = sensor.wakeUp();
+  if (error != NO_ERROR) {
+    Serial.print("Error trying to execute wakeUp(): ");
+    errorToString(error, errorMessage, sizeof errorMessage);
+    Serial.println(errorMessage);
+  }
+  error = sensor.stopPeriodicMeasurement();
+  if (error != NO_ERROR) {
+      Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
+      errorToString(error, errorMessage, sizeof errorMessage);
+      Serial.println(errorMessage);
+  }
+  error = sensor.reinit();
+  if (error != NO_ERROR) {
+      Serial.print("Error trying to execute reinit(): ");
+      errorToString(error, errorMessage, sizeof errorMessage);
+      Serial.println(errorMessage);
+  }
+  // Read out information about the sensor
+  error = sensor.getSerialNumber(serialNumber);
+  if (error != NO_ERROR) {
+      Serial.print("Error trying to execute getSerialNumber(): ");
+      errorToString(error, errorMessage, sizeof errorMessage);
+      Serial.println(errorMessage);
+      return;
+  }
+  Serial.print("serial number: ");
+  PrintUint64(serialNumber);
+  Serial.println();
+    //
+    // If temperature offset and/or sensor altitude compensation
+    // is required, you should call the respective functions here.
+    // Check out the header file for the function definitions.
+    // Start periodic measurements (5sec interval)
+    error = sensor.startPeriodicMeasurement();
+    if (error != NO_ERROR) {
+        Serial.print("Error trying to execute startPeriodicMeasurement(): ");
+        errorToString(error, errorMessage, sizeof errorMessage);
+        Serial.println(errorMessage);
+        return;
+    }
+    // If low-power mode is required, switch to the low power
+    // measurement function instead of the standard measurement
+    // function above. Check out the header file for the definition.
+    // For SCD41, you can also check out the single shot measurement example.
+
+// Screen initialization
+  tft.begin(); 
+  tft.setRotation(3); 
   tft.fillScreen(TFT_BLACK); // Clear screen
 }
 
 void loop() {
-  tft.setTextSize(2);           // Set text size
-  tft.setTextColor(TFT_WHITE);  // Set text color
-  tft.setCursor(50, 100);      // Set text position
-  tft.print("Hello, world!");  // Print text on screen
-  delay(2000); // Wait 2 seconds before repeating
+  // Sensor
+  bool dataReady = false;
+    uint16_t co2Concentration = 0;
+    float temperature = 0.0;
+    float relativeHumidity = 0.0;
+    //
+    // Slow down the sampling to 0.2Hz.
+    //
+    error = sensor.getDataReadyStatus(dataReady);
+    if (error != NO_ERROR) {
+        Serial.print("Error trying to execute getDataReadyStatus(): ");
+        errorToString(error, errorMessage, sizeof errorMessage);
+        Serial.println(errorMessage);
+        return;
+    }
+    while (!dataReady) {
+        delay(100);
+        error = sensor.getDataReadyStatus(dataReady);
+        if (error != NO_ERROR) {
+            Serial.print("Error trying to execute getDataReadyStatus(): ");
+            errorToString(error, errorMessage, sizeof errorMessage);
+            Serial.println(errorMessage);
+            return;
+        }
+    }
+    //
+    // If ambient pressure compenstation during measurement
+    // is required, you should call the respective functions here.
+    // Check out the header file for the function definition.
+    error =
+        sensor.readMeasurement(co2Concentration, temperature, relativeHumidity);
+    if (error != NO_ERROR) {
+        Serial.print("Error trying to execute readMeasurement(): ");
+        errorToString(error, errorMessage, sizeof errorMessage);
+        Serial.println(errorMessage);
+        return;
+    }
+    //
+    // Print results in physical units.
+    Serial.print("CO2 concentration [ppm]: ");
+    Serial.print(co2Concentration);
+    Serial.println();
+    Serial.print("Temperature [°C]: ");
+    Serial.print(temperature);
+    Serial.println();
+    Serial.print("Relative Humidity [RH]: ");
+    Serial.print(relativeHumidity);
+    Serial.println();
+
+
+  // Display
+  tft.setTextSize(2);           
+  tft.setTextColor(TFT_WHITE);  
+  tft.setCursor(20, 50);      
+  tft.print("CO2 [ppm]: ");
+  tft.print(co2Concentration);
+
+  tft.setTextSize(2);           
+  tft.setTextColor(TFT_WHITE);  
+  tft.setCursor(20, 100 );      
+  tft.print("Temp. [°C]: ");
+  tft.print(temperature);
+
+  tft.setTextSize(2);           
+  tft.setTextColor(TFT_WHITE);  
+  tft.setCursor(20, 150);      
+  tft.print("Humidity [RH]: ");
+  tft.print(relativeHumidity);
+  delay(5000);
+  tft.fillScreen(TFT_BLACK);  // clear screen
 }
