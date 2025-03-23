d4b3586ad7c8cedf636a187c8f35ec6bb04d9d25
wolfez <wolfderechter@gmail.com>
2025-03-23 16:29:35 +0100

    Move secrets.h file

---
+++ pkgs/firmware/include/Secrets.h
@@ -0,0 +1,8 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// Fill in WIFI credentials
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#endif
--- pkgs/firmware/src/Secrets.h
+++
@@ -1,7 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-#define WIFI_SSID "" // Change this value
-#define WIFI_PASSWORD "" // Change this value
-
-#endif
