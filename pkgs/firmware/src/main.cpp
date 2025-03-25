f9155e8f278d731e5db2ae0106e59f835ad5f8d2
wolfez <wolfderechter@gmail.com>
2025-03-25 20:19:11 +0100

    send latest_reading and history to new clients that connect

--- pkgs/firmware/src/main.cpp
+++ pkgs/firmware/src/main.cpp
@@ -147,6 +147,9 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
   case WS_EVT_CONNECT:
     Serial.printf("WebSocket client #%u connected from %s\n", client->id(),
                   client->remoteIP().toString().c_str());
+    // Send latest_reading and history on client connect
+    sendLatestReading();
+    sendHistory();
     break;
   case WS_EVT_DISCONNECT:
     Serial.printf("WebSocket client #%u disconnected\n", client->id());
