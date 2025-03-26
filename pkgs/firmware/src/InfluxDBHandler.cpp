2b9772f4ce2e4907c59ab65a5630bd703d5049c2
Andreo Van Onckelen <andreo141mlg@gmail.com>
2025-03-26 01:59:04 +0100

    remove redundant code

--- pkgs/firmware/src/InfluxDBHandler.cpp
+++ pkgs/firmware/src/InfluxDBHandler.cpp
@@ -1,26 +1,19 @@
 #include "InfluxDBHandler.h"
 
-unsigned long lastInfluxWrite = 0;
-
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
 Point influx("carbo measurements");
 
 void setupInfluxDB() {
-  // Connect to InfluxDB or test the connection
   if (!client.validateConnection()) {
     Serial.println("InfluxDB connection failed");
     while (1)
-      ; // Stop execution if connection fails
+      ;
   }
   Serial.println("InfluxDB connected!");
 }
 
 void sendReadingToInfluxDB() {
   // Clear previous fields and add new ones
-  influx.clearFields();
-
-  // Handle InfluxDB writing
-  lastInfluxWrite = millis();
   influx.clearFields();
   influx.addField("co2", co2);
   influx.addField("temperature", temperature);
