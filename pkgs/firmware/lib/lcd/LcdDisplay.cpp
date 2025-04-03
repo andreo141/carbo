2778f640118b8fd4ed08a8df33d73c07a30419a3
Andreo Van Onckelen <andreo141mlg@gmail.com>
2025-04-03 09:49:22 +0200

    wip: center co2 values

--- pkgs/firmware/lib/lcd/LcdDisplay.cpp
+++ pkgs/firmware/lib/lcd/LcdDisplay.cpp
@@ -17,40 +17,34 @@ uint16_t getCO2Color(uint16_t co2) {
 void LcdDisplay::begin() {
   tft.begin();
   tft.setRotation(3);
+  screenWidth = tft.width();
+  screenHeight = tft.height();
   tft.fillScreen(TFT_BLACK);
   tft.setFreeFont(&FreeMonoBold12pt7b);
   tft.setTextSize(1);
+
 }
 
 void LcdDisplay::clear() { tft.fillScreen(TFT_BLACK); }
 
 void LcdDisplay::drawStaticContent() {
-  drawLabel("CO2 [ppm]:", LABEL_X, FIRST_ROW_Y);
-  drawLabel("Temp [°C]:", LABEL_X,
-            FIRST_ROW_Y + ROW_SPACING); // fixme: degree sign not working
-  drawLabel("Humid [%]:", LABEL_X, FIRST_ROW_Y + 2 * ROW_SPACING);
+  // drawLabel("ppm", (screenWidth -  / 2), 170);
 }
 
 void LcdDisplay::updateValues(uint16_t co2, float temperature, uint16_t humidity) {
-  int textHeight = tft.fontHeight();
-  int valueHeight =
-      textHeight +
-      5; // Add a little extra pixels to account for any spacing or margin
-  for (int i = 0; i < 3; i++) {
-    tft.fillRect(VALUE_X, FIRST_ROW_Y - textHeight + i * ROW_SPACING,
-                 VALUE_WIDTH, valueHeight, TFT_BLACK);
-  }
-
   tft.setTextColor(getCO2Color(co2));
-  tft.setCursor(VALUE_X, FIRST_ROW_Y);
+  tft.setTextSize(3);
+
+  String co2String = String(co2);
+  int16_t co2ValueWidth = tft.textWidth(co2String);
+  int16_t co2ValueHeight = tft.fontHeight();
+  int16_t co2Baseline = tft.fontHeight() * 0.8;
+
+  tft.fillRect((screenWidth - co2ValueWidth) / 2, (screenHeight - co2ValueHeight) / 2, co2ValueWidth, co2ValueHeight, TFT_BLACK);
+  tft.setCursor((screenWidth - co2ValueWidth) / 2, (screenHeight - co2ValueHeight) / 2 + co2Baseline);
   tft.print(co2);
 
-  tft.setTextColor(TFT_WHITE);
-  tft.setCursor(VALUE_X, FIRST_ROW_Y + ROW_SPACING);
-  tft.print(temperature, 1);
 
-  tft.setCursor(VALUE_X, FIRST_ROW_Y + 2 * ROW_SPACING);
-  tft.print(humidity, 1);
 }
 
 void LcdDisplay::drawLabel(const char *label, int16_t x, int16_t y) {
--- pkgs/firmware/lib/lcd/LcdDisplay.h
+++ pkgs/firmware/lib/lcd/LcdDisplay.h
@@ -10,13 +10,10 @@ public:;
     void clear();
     void drawStaticContent();
     void updateValues(uint16_t co2, float temperature, uint16_t humidity);
-    static constexpr int LABEL_X = 20;
-    static constexpr int VALUE_X = 170;
-    static constexpr int FIRST_ROW_Y = 80;
-    static constexpr int ROW_SPACING = 50;
-    static constexpr int VALUE_WIDTH = 120;
 private:
     TFT_eSPI tft;
+    int16_t screenWidth;
+    int16_t screenHeight;
     void drawLabel(const char* label, int16_t x, int16_t y);
 };
 
