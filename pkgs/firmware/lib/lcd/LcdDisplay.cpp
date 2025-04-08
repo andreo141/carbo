479ead5ca5b35b4c1868967be8f7ae183dafab01
Andreo Van Onckelen <andreo141mlg@gmail.com>
2025-04-08 23:26:29 +0200

    center co2 value + ppm label

--- pkgs/firmware/lib/lcd/LcdDisplay.cpp
+++ pkgs/firmware/lib/lcd/LcdDisplay.cpp
@@ -20,7 +20,7 @@ void LcdDisplay::begin() {
   screenWidth = tft.width();
   screenHeight = tft.height();
   tft.fillScreen(TFT_BLACK);
-  tft.setFreeFont(&FreeMonoBold12pt7b);
+  tft.setFreeFont(&FreeSans12pt7b);
   tft.setTextSize(1);
 
 }
@@ -28,23 +28,26 @@ void LcdDisplay::begin() {
 void LcdDisplay::clear() { tft.fillScreen(TFT_BLACK); }
 
 void LcdDisplay::drawStaticContent() {
-  // drawLabel("ppm", (screenWidth -  / 2), 170);
+  const char* label = "ppm";
+  int16_t labelWidth = tft.textWidth(label);
+  drawLabel(label, (screenWidth - labelWidth) / 2 , 180);
 }
 
 void LcdDisplay::updateValues(uint16_t co2, float temperature, uint16_t humidity) {
+  
   tft.setTextColor(getCO2Color(co2));
   tft.setTextSize(3);
 
   String co2String = String(co2);
-  int16_t co2ValueWidth = tft.textWidth(co2String);
+  int16_t maxCo2ValueWidth = tft.textWidth("9999."); // point adds extra margin on right 
   int16_t co2ValueHeight = tft.fontHeight();
-  int16_t co2Baseline = tft.fontHeight() * 0.8;
-
-  tft.fillRect((screenWidth - co2ValueWidth) / 2, (screenHeight - co2ValueHeight) / 2, co2ValueWidth, co2ValueHeight, TFT_BLACK);
-  tft.setCursor((screenWidth - co2ValueWidth) / 2, (screenHeight - co2ValueHeight) / 2 + co2Baseline);
+  int16_t x = (screenWidth - maxCo2ValueWidth) / 2;
+  int16_t y = (screenHeight - co2ValueHeight) / 2;
+  int16_t baselineOffset = tft.fontHeight() * 0.8;
+  
+  tft.fillRect(x, y, maxCo2ValueWidth, co2ValueHeight, TFT_BLACK);
+  tft.setCursor(x, y + baselineOffset);
   tft.print(co2);
-
-
 }
 
 void LcdDisplay::drawLabel(const char *label, int16_t x, int16_t y) {
